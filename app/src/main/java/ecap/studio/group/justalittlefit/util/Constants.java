a3ed01092906ec3d96cbd9b08e9f3548135fe02c
Triest Montel Smart <triestmsmart@gmail.com>
2015-06-18 20:39:15 -0500

    constants and utils

---
+++ app/src/main/java/ecap/studio/group/justalittlefit/util/Constants.java
@@ -0,0 +1,81 @@
+package ecap.studio.group.justalittlefit.util;
+
+import android.os.Build;
+
+/**
+ * Constants class for app logic.
+ */
+public class Constants {
+
+    /** String code for pounds (lbs) */
+    public static final String LBS = "LBS";
+
+    /** String code for kilograms (KGS) */
+    public static final String KGS = "KGS";
+
+    /** String code for bodyweight */
+    public static final String BODY = "BODY";
+
+    /** String code for not applicable (N/A) */
+    public static final String NA = "NA";
+
+    /** String code for reps */
+    public static final String REPS = "REPS";
+
+    /** String code for timer */
+    public static final String TIMED = "T";
+
+    /** String code for logged time */
+    public static final String LOGGED_TIMED = "LT";
+
+    public static final String TODAY = "Today";
+
+    public static final String CREATE_EDIT = "Create/Edit";
+
+    public static final String ASSIGN = "Assign";
+
+    public static final String VIEW_TEXT = "View";
+
+    public static final String CREATE_EDIT_NO_SPACE = "CreateEdit";
+
+    public static final String CREATE_EDIT_FRAG = "CreateEditFrag";
+
+    public static final String ASSIGN_DIALOG = "AssignDialog";
+
+    public static final String EMPTY_STRING = "";
+
+    public static final String SPACE = " ";
+
+    public static final String OPEN_BOLD = "<b>";
+
+    public static final String CLOSE_BOLD = "</b>";
+
+    public static final String OPEN_ITALICS = "<i>";
+
+    public static final String CLOSE_ITALICS = "</i>";
+
+    public static final String TO = " to ";
+
+    public static final String ADD = "Add";
+
+    public static final String DELETE = "Delete";
+
+    public static final String BACK = "Back";
+
+    public static final String DISCARD_ALL = "Discard All";
+
+    public static final String DISCARD = "Discard";
+
+    public static final String CUSTOM_FONT_TTF = "ICBMSS20.ttf";
+
+    public static final String BLACK_GRAY = "#323232";
+
+    public static final String PRIMARY_GRAY = "#bebebe";
+
+    public static final String HIGHLIGHT_GRAY = "#696969";
+
+    public static final String BG_GRAY = "#eeeeee";
+
+    public static final int ANDROID_SDK_INT = Build.VERSION.SDK_INT;
+}
+
---
+++ app/src/main/java/ecap/studio/group/justalittlefit/util/Utils.java
@@ -0,0 +1,76 @@
+package ecap.studio.group.justalittlefit.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import ecap.studio.group.justalittlefit.model.Workout;
+
+/**
+ * Class that houses common utility methods needed by app.
+ */
+public class Utils {
+    public static String ensureValidString(String text) {
+        if (text == null || text.isEmpty()) {
+            return Constants.EMPTY_STRING;
+        } else {
+            return text;
+        }
+    }
+
+    public static boolean isCurrentActivity(String currentActivity, String requestedActivity) {
+        String trimmedCurrentActivity =
+                Utils.ensureValidString((currentActivity)).trim();
+        String trimmedRequestedActivity =
+                Utils.ensureValidString((requestedActivity)).trim();
+        return (trimmedCurrentActivity.equals(trimmedRequestedActivity));
+    }
+
+    public static boolean collectionIsNullOrEmpty(Collection collection) {
+        return (collection == null || collection.isEmpty());
+    }
+
+    public static void displayLongToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static void displayShortToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static boolean androidIsLessThanThisVersion(int version) {
+        if (Constants.ANDROID_SDK_INT >= version) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int getNumberOfDigits(int number) {
+        return (int)Math.floor(Math.log10(number) + 1);
+    }
+
+    public static void reorderWorkouts(List<Workout> workouts) {
+        Iterator<Workout> iterator = workouts.iterator();
+
+        while(iterator.hasNext()) {
+            Workout w = iterator.next();
+            w.setOrderNumber(workouts.indexOf(w));
+        }
+    }
+
+    public static ArrayList<DateTime> dateListToDateTimeList(List<Date> dates) {
+        ArrayList<DateTime> dateTimes = new ArrayList<>();
+        for (Date date : dates) {
+            dateTimes.add(new DateTime(date));
+        }
+        return dateTimes;
+    }
+}
