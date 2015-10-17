d1d762581b756ef6c1b6e8b09c290905fedf1bac
Triest Montel Smart <smart.triest@heb.com>
2015-10-17 12:52:21 -0500

    Today activity - multiple dates handled

--- app/src/main/java/ecap/studio/group/justalittlefit/adapter/WorkoutRvNameAdapter.java
+++ app/src/main/java/ecap/studio/group/justalittlefit/adapter/WorkoutRvNameAdapter.java
@@ -1,16 +1,22 @@
 package ecap.studio.group.justalittlefit.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
 import ecap.studio.group.justalittlefit.R;
+import ecap.studio.group.justalittlefit.activity.TodayActivity;
 import ecap.studio.group.justalittlefit.model.Workout;
+import ecap.studio.group.justalittlefit.util.Constants;
+import ecap.studio.group.justalittlefit.util.Utils;
 
 public class WorkoutRvNameAdapter extends RecyclerView.Adapter<WorkoutRvNameAdapter.ViewHolder> {
 
@@ -26,13 +32,28 @@ public class WorkoutRvNameAdapter extends RecyclerView.Adapter<WorkoutRvNameAdap
     public WorkoutRvNameAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.rv_workout_title_row, parent, false);
-        return new ViewHolder(v);
+
+        WorkoutRvNameAdapter.ViewHolder viewHolder = new ViewHolder(v, new ViewHolder.ViewHolderClick() {
+            @Override
+            public void onRvRowClick(View caller, Workout workoutObj) {
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(Constants.WORKOUT, workoutObj);
+
+                Intent intent = new Intent(context, TodayActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(WorkoutRvNameAdapter.ViewHolder holder, int position) {
         Workout workout = workouts.get(position);
         holder.workoutName.setText(workout.getName());
+        holder.workout = workout;
     }
 
     @Override
@@ -40,12 +61,28 @@ public class WorkoutRvNameAdapter extends RecyclerView.Adapter<WorkoutRvNameAdap
         return workouts == null ? 0 : workouts.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView workoutName;
+        public RelativeLayout container;
+        public Workout workout;
+        public ViewHolderClick mListener;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, ViewHolderClick viewHolderClick) {
             super(itemView);
             workoutName = (TextView) itemView.findViewById(R.id.tvWorkoutTitle);
+            mListener = viewHolderClick;
+            container = (RelativeLayout) itemView.findViewById(R.id.rlRvContainer);
+            container.setOnClickListener(this);
+            workoutName.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onRvRowClick(v, workout);
+        }
+
+        public static interface ViewHolderClick {
+            public void onRvRowClick(View caller, Workout workoutObj);
         }
     }
 }
